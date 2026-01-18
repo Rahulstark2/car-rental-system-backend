468c1fd575cd366e008ec0233f0b7e90bf51ac4f
Rahulstark2 <rahulstark2@gmail.com>
2026-01-18 16:11:43 +0530

    final commit

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import express from 'express'
 import authRoutes from './auth/routes.js'
 import bookingRoutes from './bookings/routes.js'
-const app=express()
+import { rateLimit } from 'express-rate-limit'
+
+const app = express()
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+app.use(limiter)
 app.use(express.json())
-app.use('/auth',authRoutes)
-app.use('/bookings',bookingRoutes)
+app.use('/auth', authRoutes)
+app.use('/bookings', bookingRoutes)
 export default app
--- src/auth/controller.ts
+++ src/auth/controller.ts
@@ -7,6 +7,7 @@ import { err } from '../utils/error.js'
 export const signup = async (req: Request, res: Response) => {
   const { username, password } = req.body
   if (!username || !password) return err(res, 400, 'invalid inputs')
+  if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/.test(password)) return err(res, 400, 'password weak')
   const u = await prisma.user.findUnique({ where: { username } })
   if (u) return err(res, 409, 'username already exists')
   const hash = await bcrypt.hash(password, 10)
@@ -18,9 +19,9 @@ export const login = async (req: Request, res: Response) => {
   const { username, password } = req.body
   if (!username || !password) return err(res, 400, 'invalid inputs')
   const u = await prisma.user.findUnique({ where: { username } })
-  if (!u) return err(res, 401, 'user does not exist')
-  const ok = await bcrypt.compare(password, u.password)
-  if (!ok) return err(res, 401, 'incorrect password')
+  if (!u || !(await bcrypt.compare(password, u.password))) {
+    return err(res, 401, 'Invalid credentials')
+  }
   const token = jwt.sign({ userId: u.id, username }, process.env.JWT_SECRET!)
   res.json({ success: true, data: { message: 'Login successful', token } })
 }
