555448b7d3bab5ef073bee66712421173047421c
Rahulstark2 <rahulstark2@gmail.com>
2026-01-18 12:26:00 +0530

    fix prisma.ts

---
+++ prisma/migrations/20260118065228_init/migration.sql
@@ -0,0 +1,31 @@
+-- CreateEnum
+CREATE TYPE "BookingStatus" AS ENUM ('booked', 'completed', 'cancelled');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Booking" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "car_name" TEXT NOT NULL,
+    "days" INTEGER NOT NULL,
+    "rentPerDay" INTEGER NOT NULL,
+    "status" "BookingStatus" NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Booking_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
